85eeb600ccb93a6a25a88ed1ab776d85b7cf9e21
Karalee Shotola <karalee@utexas.edu>
2018-08-03 11:24:02 +0200

    Added state for locations and markers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Map from './Map';
-import List from './List';
+//import List from './List';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    locations: [],
+    markers: []
+  }
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map
+           locations={this.state.locations}
+           markers={this.state.markers}
+        />
       </div>
     );
   }
