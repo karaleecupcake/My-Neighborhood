b58c7894c5e2663b62dcef07c8df26580f7f3d9b
Karalee Shotola <karalee@utexas.edu>
2018-08-03 11:18:33 +0200

    Added array of locations

--- src/Location.js
+++ src/Location.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class Location extends Component {
+
+  locations = [
+    {title: 'Thorvaldsens Museum', id: '1', location: {lat: 55.67657903345543, lng: 12.577859624454172}},
+    {title: 'Statens Museum for Kunst - SMK', id: '2', location: {lat: 55.68857458177556, lng: 12.578398880888566}},
+    {title: 'Geologisk Museum', id: '3', location: {lat: 55.68727598191482, lng: 12.577009014284597}},
+    {title: 'Nationalmuseet', id: '4', location: {lat: 55.67467858465465, lng: 12.574479097171546}},
+    {title: 'Dansk Jødisk Museum', id: '5', location: {lat: 55.67448651201848, lng: 12.582068860323538}},
+    {title: 'Danish Arsenal Museum', id: '6', location: {lat: 55.68738073048422, lng: 12.578379040282336}},
+    {title: 'Kunsthal Charlottenborg', id: '7', location: {lat: 55.67992619015504, lng: 12.588278526489983}},
+    {title: 'Designmuseum Danmark', id: '8', location: {lat: 55.68667015018599, lng: 12.592298417358537}},
+    {title: 'Zoologisk Museum', id: '9', location: {lat: 55.70236913592865, lng: 12.559144797677261}},
+    {title: 'Magasin du Nord Museum', id: '10', location: {lat: 55.6786994934082, lng: 12.584016799926758}}
+  ]
+
   render () {
     return (
-
+      <div>
+      </div>
     );
   }
 }
