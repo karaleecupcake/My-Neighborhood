59ae513e2bc280ac5bb55e16175f2699e595ed29
Karalee Shotola <karalee@utexas.edu>
2018-08-08 12:54:00 +0200

    Added id to locations

--- src/Location.js
+++ src/Location.js
@@ -1,11 +1,11 @@
 export const locations = [
-      {title: 'Thorvaldsens Museum', location: {lat: 55.67657903345543, lng: 12.577859624454172}},
-      {title: 'Statens Museum for Kunst - SMK', location: {lat: 55.68857458177556, lng: 12.578398880888566}},
-      {title: 'Geologisk Museum', location: {lat: 55.68727598191482, lng: 12.577009014284597}},
-      {title: 'Nationalmuseet', location: {lat: 55.67467858465465, lng: 12.574479097171546}},
-      {title: 'Dansk Jødisk Museum',location: {lat: 55.67448651201848, lng: 12.582068860323538}},
-      {title: 'Danish Arsenal Museum', location: {lat: 55.68738073048422, lng: 12.578379040282336}},
-      {title: 'Kunsthal Charlottenborg', location: {lat: 55.67992619015504, lng: 12.588278526489983}},
-      {title: 'Designmuseum Danmark', location: {lat: 55.68667015018599, lng: 12.592298417358537}},
-      {title: 'Magasin du Nord Museum', location: {lat: 55.6786994934082, lng: 12.584016799926758}}
+      {title: 'Thorvaldsens Museum', id: '4adcdafdf964a520c25d21e3', location: {lat: 55.67657903345543, lng: 12.577859624454172}},
+      {title: 'Statens Museum for Kunst - SMK', id: '4adcdafdf964a520e95d21e3', location: {lat: 55.68857458177556, lng: 12.578398880888566}},
+      {title: 'Geologisk Museum', id: '4adcdafef964a520055e21e3', location: {lat: 55.68727598191482, lng: 12.577009014284597}},
+      {title: 'Nationalmuseet', id: '4adcdafef964a520ef5d21e3', location: {lat: 55.67467858465465, lng: 12.574479097171546}},
+      {title: 'Dansk Jødisk Museum', id: '4b9798e4f964a520cf0a35e3', location: {lat: 55.67448651201848, lng: 12.582068860323538}},
+      {title: 'Danish Arsenal Museum', id: '53f485d9498e15557bc86af7', location: {lat: 55.68738073048422, lng: 12.578379040282336}},
+      {title: 'Kunsthal Charlottenborg', id: '4adcdafdf964a520e85d21e3', location: {lat: 55.67992619015504, lng: 12.588278526489983}},
+      {title: 'Designmuseum Danmark', id: '4dca63b345dd19439a087426', location: {lat: 55.68667015018599, lng: 12.592298417358537}},
+      {title: 'Magasin du Nord Museum', id: '564b0a83498eacbe22b1b498', location: {lat: 55.6786994934082, lng: 12.584016799926758}}
     ]
