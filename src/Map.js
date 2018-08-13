f9a2e4c6d652c82d03eb53af7e805f714e115ecf
Karalee Shotola <karalee@utexas.edu>
2018-08-13 11:29:18 +0200

    Added markers and info windows data from FourSquare

--- src/Map.js
+++ src/Map.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
-import { locations } from './Location';
 
 class Map extends Component {
 
-  state = {
-    locations: locations
-  }
-
   render () {
 
+// Uses react-google-maps to create a Google Map with markers and info windows from the FourSquare API
     const CopenhagenMap = withScriptjs(withGoogleMap((props) =>
        <GoogleMap
-          defaultZoom={ 14 }
-          center={{ lat: 55.679452, lng: 12.580216999999948 }}>
+          defaultZoom={ 13 }
+          center={{ lat: 55.6803344, lng: 12.5847101 }}>
 
           {this.props.venues.map((venue, index) =>
              <Marker
-                key={location.index}
-                id={location.id}
-                title={location.title}
-                position={{lat: location.location.lat, lng: location.location.lng}}
-                onClick={this.props.openWindow}>
-                  <InfoWindow
-                  onCloseClick={this.props.closeWindow}>
-                  <div className="info-window">
-                     <h4>{location.title}</h4>
-                     <p>{venue.location.formattedAddress}</p>
-                  </div>
-              </InfoWindow>
-            )}
+                key={index}
+                id={venue.id}
+                name={venue.name}
+                position={{lat: venue.location.lat, lng: venue.location.lng}}
+                onClick={() => {this.props.toggleWindow(index)}}
+                 animation={this.props.selectedLocation == index ? window.google.maps.Animation.DROP: null}>
 
-
+                  {this.props.selectedLocation === index && (
+                    <InfoWindow
+                    onCloseClick={props.onToggleOpen}>
+                    <div className="info-window">
+                       <h4>{venue.name}</h4>
+                       <p>{venue.location.formattedAddress[0]}</p>
+                       <p>{venue.location.formattedAddress[1]}</p>
+                       <p>{venue.location.formattedAddress[2]}</p>
+                    </div>
+                </InfoWindow>
+              )}
 
             </Marker>
           )}
@@ -40,10 +39,10 @@ class Map extends Component {
 
     return (
       <div className="google-map">
-         <div className="map-title">
-            <h1>Copenhagen, Denmark</h1>
-         </div>
-         <div className="copenhagen-map">
+         <header>
+            <h1>Museums in Copenhagen, Denmark</h1>
+         </header>
+         <div className="copenhagen-map" role="application">
             <CopenhagenMap
                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIMSlxPLSZiskk6up0GprtdKpQzUXthNE&v=3.exp&libraries=geometry,drawing,places"
                loadingElement={<div style={{ height: `100%` }} />}
@@ -51,6 +50,9 @@ class Map extends Component {
                mapElement={<div style={{ height: `100%` }} />}
                />
          </div>
+         <footer className="footer">
+            <p>Created by Karalee Shotola and integrated with FourSquare API</p>
+         </footer>
       </div>
     );
   }
