10dcdf1df07ef0801ace4d21b31c638602dba4a8
Karalee Shotola <karalee@utexas.edu>
2018-08-13 11:29:38 +0200

    Added list component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import Map from './Map';
-//import List from './List';
+import List from './List';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    locations: [],
     venues: [],
-    selectedVenue: {},
-    isOpen: false,
+    selectedVenue: {}
   }
 
+// Fetches data for museums from FourSquare and handles errors
   componentDidMount() {
-    fetch('https://api.foursquare.com/v2/venues/search?ll=55.679452,12.580216999999948&intent=browse&radius=10000&query=museum&client_id=0V25KPKE3QMUS3BPCJBUABV5KJMFFD33UQGDFOEGHTJFQF2J&client_secret=332MWGWQZIATESF1GIGOSQX2KI2NRZZRPIPKHJPZJARUN3VC&v=20180802')
+    fetch('https://api.foursquare.com/v2/venues/search?ll=55.6760968,12.5683372&intent=browse&radius=10000&query=museum&client_id=0V25KPKE3QMUS3BPCJBUABV5KJMFFD33UQGDFOEGHTJFQF2J&client_secret=332MWGWQZIATESF1GIGOSQX2KI2NRZZRPIPKHJPZJARUN3VC&v=20180802')
     .then(response => response.json())
     .then((data) => {
       this.setState({
@@ -25,22 +24,16 @@ class App extends Component {
       console.log('Error getting data from the FourSquare API. Check the key.')
     })
 
+// Handles errors for Google Maps
     window.gm_authFailure = () => {
       alert('Error loading Google Maps. Check the API key.')
     }
   }
 
-  openWindow = (selectedVenue) => {
+// Toogles info window when venue is clicked
+  toggleWindow = selectedVenue => {
     this.setState({
-      selectedVenue: selectedVenue,
-      isOpen: true
-    })
-  }
-
-  closeWindow = () => {
-    this.setState({
-      selectedVenue: {},
-      isOpen: false
+      selectedLocation: selectedVenue
     })
   }
 
@@ -49,12 +42,13 @@ class App extends Component {
     return (
       <div className="App" role="main">
         <Map
-           locations = {this.state.locations}
            venues = {this.state.venues}
-           address = {this.state.address}
-           selectedVenue = {this.state.selectedVenue}
-           openWindow = {this.openWindow}
-           closeWindow = {this.closeWindow}
+           selectedLocation = {this.state.selectedLocation}
+           toggleWindow = {this.toggleWindow}
+        />
+        <List
+           venues = {this.state.venues}
+           toggleWindow = {this.toggleWindow}
         />
       </div>
     );
