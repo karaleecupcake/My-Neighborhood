d0db27ab1440047cfe3d274de95647779019b279
Karalee Shotola <karalee@utexas.edu>
2018-08-14 10:45:55 +0200

    Added react-burger-menu

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu'
 
 class List extends Component {
   state = {
@@ -15,6 +16,7 @@ class List extends Component {
     const venueList = this.props.venues.map((venue, index) => (
       <li className= "museum"
          key={index}
+         tabIndex={0}
          onClick={() => {this.props.toggleWindow(index)}}
          onKeyPress={() => {this.props.toggleWindow(index)}}
          >
@@ -23,20 +25,22 @@ class List extends Component {
     ))
 
     return (
-      <div className="search">
-         <input
-            type="text"
-            placeholder="Search for museum"
-            value={this.state.query}
-            onChange={(event) => this.updateQuery(event.target.value)}
-            role="search"
-         />
-         <div className="museumList">
-            <ul>
-               {venueList}
-            </ul>
+      <Menu>
+         <div className="search">
+            <input
+               type="text"
+               placeholder="Search for museum"
+               value={this.state.query}
+               onChange={(event) => this.updateQuery(event.target.value)}
+               role="search"
+            />
+            <div className="museumList">
+               <ul>
+                  {venueList}
+               </ul>
+            </div>
          </div>
-      </div>
+      </Menu>
     );
   }
 }
